a3832dad87e692e5ace651706e349cfdedeeaa01
Roman Polishchenko <roma.vinn@gmail.com>
2021-04-11 18:32:03 +0300

    small fix

--- tasks/models.py
+++ tasks/models.py
@@ -8,10 +8,9 @@ class Task(models.Model):
     """ Task model class """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(verbose_name='Task name', max_length=200)
-    description = models.CharField(verbose_name='Description', max_length=2000)
+    description = models.CharField(verbose_name='Description', max_length=2000, blank=True)
     type = models.IntegerField('Event or Task', choices=((0, "Event"), (1, "Task")))
-    deadline = models.DateTimeField(verbose_name='Task deadline', blank=True)
+    deadline = models.DateTimeField(verbose_name='Task deadline')
 
     def __str__(self):
         return "Task: " + self.name
-
--- tasks/services.py
+++ tasks/services.py
@@ -1,4 +1,4 @@
-from datetime import date, timedelta
+from datetime import timedelta
 from tasks.models import Task
 from django.utils import timezone
 
@@ -7,7 +7,8 @@ def get_tasks_daily(request):
 
     today = timezone.now()
 
-    tasks = Task.objects.filter(user=request.user, deadline__day=today.day).order_by('deadline')
+    tasks = Task.objects.filter(user=request.user, deadline__year=today.year,
+                                deadline__day=today.day).order_by('deadline')
 
     return tasks
 
@@ -17,7 +18,8 @@ def get_tasks_weekly(request):
     start = today - timedelta(days=today.weekday())  # the first day of the current week
     end = start + timedelta(days=6)  # the last day of the current week
 
-    tasks = Task.objects.filter(user=request.user, deadline__day__range=[start.day, end.day]).order_by('deadline')
+    tasks = Task.objects.filter(user=request.user, deadline__year=start.year, deadline__month=start.month,
+                                deadline__day__range=[start.day, end.day]).order_by('deadline')
 
     return tasks
 
@@ -27,6 +29,9 @@ def get_tasks_monthly(request):
     start = today.replace(day=1)
     end = today.replace(month=today.month+1).replace(day=1) - timedelta(days=1)
 
-    tasks = Task.objects.filter(user=request.user, deadline__day__range=[start.day, end.day]).order_by('deadline')
+    print([start, end, start.day, end.day])
+
+    tasks = Task.objects.filter(user=request.user, deadline__year=start.year, deadline__month=start.month,
+                                deadline__day__range=[start.day, end.day]).order_by('deadline')
 
     return tasks
