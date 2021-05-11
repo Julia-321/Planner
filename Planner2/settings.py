c86354069e0758543f0a0ffa0084135e696ac9b2
Roman Polishchenko <roman.polishchenko@deepxhub.com>
2021-05-11 13:59:30 +0300

    prepared for pusing onto heroku

--- Planner2/settings.py
+++ Planner2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +141,6 @@ EMAIL_HOST_PASSWORD = 'v05022000'
 EMAIL_PORT = 587
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Planner2.wsgi --log-file -
